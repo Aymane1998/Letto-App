da1a0758e89ead6d349c20c0abeb3bd26da201ea
unknown <aymane.elarache1@epsi.fr>
2020-12-17 10:57:04 +0100

    Save Changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 
 import './App.css';
-import Login from './components/Login'
 import Home from './components/Home'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import{Route, Switch , BrowserRouter as Router} from 'react-router-dom';
 function App() {
   return (
     
     
-    <Router>
+    
     <div className="App">
      
-     <Switch>
-      <Route path ="/" exact component={Home}/>
-      <Route path ="/Login" exact component={Login}/>
-     </Switch>
+     
+      <Home/>
+     
     </div>
-    </Router>
+    
     
   );
 }
